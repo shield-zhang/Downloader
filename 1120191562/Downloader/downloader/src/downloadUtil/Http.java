76753d2360bbdada19a154a9fd1f5e2423fa15f0
1120191562zhq <1120191562@bit.edu.cn>
2022-02-06 23:29:40 +0800

    Http.java新增获取文件名

--- 1120191562/Downloader/downloader/src/downloadUtil/Http.java
+++ 1120191562/Downloader/downloader/src/downloadUtil/Http.java
@@ -7,18 +7,25 @@ import java.net.URLConnection;
 
 public class Http {
 
-
-
+    /**
+     * 将下载链接转换为HttpURLConnection
+     * @return
+     * @throws
+     */
+    public static HttpURLConnection getHttpURLConnection(String urlpath) throws IOException {
+        URL url = new URL(urlpath);
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11");
+        return (HttpURLConnection) urlConnection;
+    }
 
     /**
-     *
-     * @return 下载链接的HttpURLConnection
-     * @throws IOException
+     * 获取要下载的文件名
+     * @param
+     * @return
      */
-    public static HttpURLConnection getHttpURLConnection(String urlpath) throws IOException{
-        URL url=new URL(urlpath);
-        URLConnection urlConnection = (URLConnection) url.openConnection();
-        urlConnection.setRequestProperty("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11");
-        return (HttpURLConnection) urlConnection;
+    public static String getFileName(String url){
+        int index=url.lastIndexOf("/");
+        return url.substring(index+1);
     }
 }
