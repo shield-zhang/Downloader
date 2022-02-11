4d48fe84e6301e7994897bad51cbe4c04c56c0c7
1120191562zhq <1120191562@bit.edu.cn>
2022-02-11 10:01:32 +0800

    第一阶段小改动，加入默认地址和默认线程数

--- 1120191562/Downloader/downloader/src/Main.java
+++ 1120191562/Downloader/downloader/src/Main.java
@@ -18,22 +18,34 @@ public class Main {
                 System.out.println("该链接无法访问，请重新输入:");
                 url = scanner.next();
             }
-
-            System.out.println("请输入文件保存地址：");
-            savePath = scanner.next();
-            while (!IfLegal.ifLegalSavePath(savePath)) {
-                System.out.println("该路径不存在，请重新输入！");
+            System.out.println("输入1保存路径为默认地址（即当前地址），输入2为自定义地址：");
+            int  temp=scanner.nextInt();
+            if(temp==1){
+                System.out.println("保存路径为默认地址： "+System.getProperty("user.dir"));
+                savePath=System.getProperty("user.dir");
+            }else{
+                System.out.println("请输入文件保存地址：");
                 savePath = scanner.next();
+                while (!IfLegal.ifLegalSavePath(savePath)) {
+                    System.out.println("该路径不存在，请重新输入！");
+                    savePath = scanner.next();
+                }
             }
+            System.out.println("输入1保存路径为默认线程（即8线程），输入2选择自定义线程数(1~32)：");
+            temp=scanner.nextInt();
+            if (temp==1){
+                System.out.println("您选择了8线程下载！");
+                threadNum=8;
+            }else {
+                System.out.println("请输入线程数（1~32）：");
 
-
-            System.out.println("请输入线程数（1~32）：");
-
-            threadNum = scanner.nextInt();
-            while (!IfLegal.ifLegalThreadNum(threadNum)) {
-                System.out.println("线程数目非法，请重新输入！");
                 threadNum = scanner.nextInt();
+                while (!IfLegal.ifLegalThreadNum(threadNum)) {
+                    System.out.println("线程数目非法，请重新输入！");
+                    threadNum = scanner.nextInt();
+                }
             }
+
 
             DownloadControl downloadControl = new DownloadControl();
             downloadControl.run(url,savePath,threadNum);
